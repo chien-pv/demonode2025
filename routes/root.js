904f21bf10d6803d173699a69dd5819e5b5e56d7
chienpv <phamvanbk11@gmail.com>
2025-01-16 14:44:13 +0700

    upload imgae

--- routes/root.js
+++ routes/root.js
@@ -1,11 +1,32 @@
 import express from "express";
 import HomeController from "../controllers/homeController.js";
+import multer from "multer";
+
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./public/img/");
+  },
+
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", HomeController.index);
 rootRouter.get("/products", (req, res) => {
   res.render("products");
+});
+
+rootRouter.get("/upload", (req, res) => {
+  res.render("login");
+});
+
+rootRouter.post("/upload", upload.single("image"), (req, res) => {
+  res.redirect("/");
 });
 
 rootRouter.post("/login", (req, res) => {
