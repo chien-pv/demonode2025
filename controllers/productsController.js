8852f630d65ff727e9bc254a79daa8b814473d2f
chienpv <phamvanbk11@gmail.com>
2025-02-08 15:21:34 +0700

    rest api

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,31 @@
 import Product from "../models/product.js";
 export default class ProductsController {
   static async index(req, res) {
-    let products = await Product.findAll({});
-    res.json({ data: products });
+    try {
+      let products = await Product.findAll({});
+      res.status(200).json({ data: products });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async show(req, res) {
+    try {
+      let id = req.params.id;
+      let product = await Product.findByPk(id);
+      res.status(200).json({ data: product });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async create(req, res) {
+    try {
+      let { name, description, image, price } = req.body;
+      let product = await Product.create({ name, description, image, price });
+      res.status(200).json({ message: "Tạo mới thành công!!", data: product });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,7 @@ import ProductsController from "../controllers/productsController.js";
 const productRouter = express.Router();
 
 productRouter.get("/", ProductsController.index);
+productRouter.get("/:id", ProductsController.show);
+productRouter.post("/", ProductsController.create);
 
 export default productRouter;
