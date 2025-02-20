5aa95b9490160c9da20e956272000631e4a25a45
chienpv <phamvanbk11@gmail.com>
2025-02-20 15:09:54 +0700

    login API

--- middwares/checkLogin.js
+++ middwares/checkLogin.js
@@ -1,4 +1,14 @@
+import jwt from "jsonwebtoken";
+
 export function checkLoginMiddware(req, res, next) {
   if (req.session.user) next();
   else res.redirect("/login/form");
+}
+
+export function checkLoginAPIMiddware(req, res, next) {
+  if (jwt.verify(token, "Nodejs")) next();
+  else
+    res
+      .status(400)
+      .json({ message: "Bạn cần đăng nhập để thực hiện chức năng này!!!" });
 }
--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,11 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import HomeController from "../controllers/homeController.js";
 import multer from "multer";
 import User from "../models/user.js";
 import { checkLoginMiddware } from "../middwares/checkLogin.js";
+const salt = bcrypt.genSaltSync(10);
 
 var storage = multer.diskStorage({
   destination: function (req, file, callback) {
@@ -37,7 +40,7 @@ rootRouter.post("/login", async (req, res) => {
       email,
     },
   });
-  if (user && user.password == pw) {
+  if (user && bcrypt.compareSync(pw, user.password)) {
     req.session.user = user;
     res.redirect("/");
     return;
@@ -55,5 +58,30 @@ rootRouter.get("/contact", (req, res) => {
 
 rootRouter.get("/login/form", (req, res) => {
   res.render("home/login");
+});
+
+rootRouter.post("/api/resgiter", async (req, res) => {
+  let { username, email, password } = req.body;
+
+  password = bcrypt.hashSync(password, salt);
+
+  let user = User.create({ username, email, password });
+  res.json({ message: "Đăng kí thành công!!!", user });
+});
+
+rootRouter.post("/api/login", async (req, res) => {
+  let { email, password } = req.body;
+  // console.log(req.body);
+  let user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    let token = jwt.sign({ email }, "nodejs");
+    res.json({ message: "Đăng Nhập thành công!!!", token });
+    return;
+  }
+  res.status(400).json({ message: "Đăng Nhập Thất bại!!!" });
 });
 export default rootRouter;
